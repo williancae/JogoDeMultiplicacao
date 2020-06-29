cc200b274bd31df9237347362db397e9110cc96a
williancae <williancaecam@gmail.com>
2020-06-29 19:23:34 -0300

    Mudando variaveis futuro upgrade

--- principal.py
+++ principal.py
@@ -1,3 +1,4 @@
+
 import random # importando biblioteca que gera numeros aleatorios
 import datetime
 # Dicionario de formatação de texto
@@ -24,29 +25,28 @@ print('╚═╝     ╚═╝ ╚═════╝ ╚══════╝╚
 score = 10 #pontuação inicial
 fault = 0 #iniciando a varivel de erros
 hits = 0 #iniciando a variavel de acertos
-x = datetime.datetime.now()
-print(x.strftime("\nInicio as %H:%M:%S")) #hora de inicio
+horaInicio = datetime.datetime.now()
+print(horaInicio.strftime("\nInicio as %H:%M:%S")) #hora de inicio
 while True:
     number1 = random.randint(1, 10)
-    number2 = random.randint(1, 15)
+    number2 = random.randint(1, 10)
     result = number1 * number2
     response = int(input('Qual o resultado da multiplicação:  {} x {} = '.format(number1, number2)))
     if result == response:
         score += 1
         hits += 1
         print('{} VOCÊ ACERTOU!{} {}Voce tem {} Pontos{}\n'.format(colors['green'], colors['clear'], colors['blue'], score, colors['clear']))
-        if score >= 20:
+        if score >= 15:
             print('\n{}VOCÊ GANHOU O JOGO!!!{} {}Acertou {} vezes e {} Errou {} vez(es){}'.format(post['win'], colors['clear'], colors['blue'], hits, colors['red'], fault, colors['clear']))
             break
     elif result != response:
         score -= 2
         fault += 1
-        print('{} VOCÊ ERROU!{} {}Voce tem {} Pontos{}'.format(colors['red'], colors['clear'], colors['blue'], score, colors['clear']))
+        print('{}  VOCÊ ERROU!{} {}Voce tem {} Pontos{}'.format(colors['red'], colors['clear'], colors['blue'], score, colors['clear']))
         print('{}A Resposta era {}{}\n'.format(colors['cyan'], result,colors['clear']))
         if score <= 0:
             print('\n{}VOCÊ PERDEU!!!{} Errou {}{} vezes e {} acertou {} vezes{}'.format(post['alert'], colors['clear'], colors['yellow'], fault, colors['cyan'], hits, colors['clear']))
             break
-y = datetime.datetime.now()
-print(x.strftime("\nInicio as %H:%M:%S")) #hora de inicio
-print(y.strftime("\nTerminou as %H:%M:%S")) #hora de fim
-
+horaFim = datetime.datetime.now()
+print(horaInicio.strftime("\nInicio as %H:%M:%S")) #hora de inicio
+print(horaFim.strftime("\nTerminou as %H:%M:%S")) #hora de 
